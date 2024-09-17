c72db2cefb8a38ca93a6431d8dd7b6392da7758f
LuisAl2589 <61215951+LuisAl2589@users.noreply.github.com>
2024-09-17 17:41:09 -0600

    Se inicializo el proyecto con spring

---
+++ cerradurasWeb/cerradurasWeb/src/test/java/ipn/mx/cerradurasWeb/CerradurasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ipn.mx.cerradurasWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerradurasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
